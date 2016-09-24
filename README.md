30d407920f0e2b7228a633b70241e18f054ce509
Milos Zivojinovic <miloszivojinovic93@gmail.com>
2016-09-24 15:14:50 +0200

    sync with latest bootstrap_manual_skin built-in version in b2evolution 6.7.6-stable

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Link to skin: http://skins.b2evolution.net/bootstrap-manual-skin
 
 <img src="skinshot-big.png"/>
+
+### Update Log:
+
+#### v6.7.6-stable
+-sync with latest bootstrap_manual_skin built-in version in b2evolution 6.7.6-stable
--- help.main.php
+++ help.main.php
@@ -1,20 +1,23 @@
 <?php
 /**
- * This is the 404 page template for the "bootstrap_manual" skin.
+ * This is the template that displays the help screen for a collection
  *
- * This skin only uses one single template which includes most of its features.
- * It will also rely on default includes for specific dispays (like the comment form).
+ * This file is not meant to be called directly.
+ * It is meant to be called by an include in the main.page.php template.
+ * To display the archive directory, you should call a stub AND pass the right parameters
+ * For example: /blogs/index.php?disp=help
  *
- * For a quick explanation of b2evo 2.0 skins, please start here:
- * {@link http://b2evolution.net/man/skin-development-primer}
- *
- * The main page template is used to display the blog when no specific page template is available
- * to handle the request (based on $disp).
+ * b2evolution - {@link http://b2evolution.net/}
+ * Released under GNU GPL License - {@link http://b2evolution.net/about/gnu-gpl-license}
+ * @copyright (c)2003-2016 by Francois Planque - {@link http://fplanque.com/}
  *
  * @package evoskins
- * @subpackage bootstrap_manual
+ * @subpackage bootstrap_manual_skin
  */
 if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
+
+
+global $app_version, $disp, $Skin, $Settings, $Blog;
 
 if( evo_version_compare( $app_version, '6.4' ) < 0 )
 { // Older skins (versions 2.x and above) should work on newer b2evo versions, but newer skins may not work on older b2evo versions.
@@ -22,22 +25,14 @@ if( evo_version_compare( $app_version, '6.4' ) < 0 )
 }
 
 
-if( ! empty( $requested_404_title ) )
-{ // Initialize a prefilled search form
-	set_param( 's', str_replace( '-', ' ', $requested_404_title ) );
-	set_param( 'sentence', 'OR' );
-	set_param( 'title', '' ); // Empty this param to exclude a filter by post_urltitle
-}
-
 // This is the main template; it may be used to display very different things.
 // Do inits depending on current $disp:
-skin_init( ! empty( $requested_404_title ) ? 'search' : $disp );
+skin_init( $disp );
 
 
 // -------------------------- HTML HEADER INCLUDED HERE --------------------------
-skin_include( '_html_header.inc.php', array() );
+skin_include( '_html_header.inc.php' );
 // -------------------------------- END OF HEADER --------------------------------
-
 
 // ---------------------------- SITE HEADER INCLUDED HERE ----------------------------
 // If site headers are enabled, they will be included here:
@@ -45,11 +40,16 @@ siteskin_include( '_site_body_header.inc.php' );
 // ------------------------------- END OF SITE HEADER --------------------------------
 ?>
 
+
 <div class="container">
 
 <header id="header" class="row<?php echo $Settings->get( 'site_skins_enabled' ) ? ' site_skins' : ''; ?>">
 
 	<div class="coll-xs-12 coll-sm-12 col-md-4 col-md-push-8">
+		<?php
+		if( $Skin->is_visible_container( 'page_top' ) )
+		{ // Display 'Page Top' widget container
+		?>
 		<div class="evo_container evo_container__page_top">
 		<?php
 			// ------------------------- "Page Top" CONTAINER EMBEDDED HERE --------------------------
@@ -67,9 +67,14 @@ siteskin_include( '_site_body_header.inc.php' );
 			// ----------------------------- END OF "Page Top" CONTAINER -----------------------------
 		?>
 		</div>
+		<?php } ?>
 	</div><!-- .col -->
 
 	<div class="coll-xs-12 col-sm-12 col-md-8 col-md-pull-4">
+		<?php
+		if( $Skin->is_visible_container( 'header' ) )
+		{ // Display 'Header' widget container
+		?>
 		<div class="evo_container evo_container__header">
 		<?php
 			// ------------------------- "Header" CONTAINER EMBEDDED HERE --------------------------
@@ -84,10 +89,15 @@ siteskin_include( '_site_body_header.inc.php' );
 			// ----------------------------- END OF "Header" CONTAINER -----------------------------
 		?>
 		</div>
+		<?php } ?>
 	</div><!-- .col -->
 
 </header><!-- .row -->
 
+<?php
+if( $Skin->is_visible_container( 'menu' ) )
+{ // Display this widget container only when it is not disabled
+?>
 <nav class="row">
 
 	<div class="col-md-12">
@@ -116,6 +126,7 @@ siteskin_include( '_site_body_header.inc.php' );
 	</div><!-- .col -->
 
 </nav><!-- .row -->
+<?php } ?>
 
 <div class="row">
 
@@ -177,82 +188,11 @@ siteskin_include( '_site_body_header.inc.php' );
 
 
 <?php
-	// --------------------- 404 CONTENT TEMPLATE INCLUDED HERE ----------------------
-	echo '<div class="error_404">';
-
-	echo '<h1>404 Not Found</h1>';
-
-	echo '<p>'.T_('The manual page you are requesting doesn\'t seem to exist (yet).').'</p>';
-
-	$post_title = '';
-	$post_urltitle = '';
-	if( ! empty( $requested_404_title ) )
-	{ // Set title & urltitle for new post
-		$post_title = str_replace( ' ', '%20', ucwords( str_replace( '-', ' ', $requested_404_title ) ) );
-		$post_urltitle = $requested_404_title;
-	}
-
-	// Button to create a new page
-	$write_new_post_url = $Blog->get_write_item_url( 0, $post_title, $post_urltitle );
-	if( ! empty( $write_new_post_url ) )
-	{ // Display button to write a new post
-		echo '<a href="'.$write_new_post_url.'" class="roundbutton roundbutton_text_noicon">'.T_('Create this page now').'</a>';
-	}
-
-	echo '<p>'.T_('You can search the manual below.').'</p>';
-
-	echo '</div>';
-
-	if( ! empty( $requested_404_title ) )
-	{ // Initialize a prefilled search form
-		skin_include( '_search.disp.php', $Skin->get_template( 'disp_params' ) );
-		// Note: You can customize the default search by copying the generic
-		// /skins/_search.disp.php file into the current skin folder.
-	}
-	else
-	{ // Display a search form with TOC
-		echo '<div class="error_additional_content">';
-		// --------------------------------- START OF SEARCH FORM --------------------------------
-		// Call the coll_search_form widget:
-		skin_widget( array(
-				// CODE for the widget:
-				'widget' => 'coll_search_form',
-				// Optional display params:
-				'block_start'          => '',
-				'block_end'            => '',
-				'title'                => T_('Search this manual:'),
-				'disp_search_options'  => 0,
-				'search_class'         => 'extended_search_form',
-				'block_title_start'    => '<h3>',
-				'block_title_end'      => '</h3>',
-				'search_class'         => 'compact_search_form',
-				'search_input_before'  => '<div class="input-group">',
-				'search_input_after'   => '',
-				'search_submit_before' => '<span class="input-group-btn">',
-				'search_submit_after'  => '</span></div>',
-			) );
-		// ---------------------------------- END OF SEARCH FORM ---------------------------------
-
-		echo '<p>'.T_('or you can browse the table of contents below:').'</p>';
-
-		// --------------------------------- START OF CONTENT HIERARCHY --------------------------------
-		echo '<h2 class="table_contents">'.T_('Table of contents').'</h2>';
-		skin_widget( array(
-				// CODE for the widget:
-				'widget' => 'content_hierarchy',
-				// Optional display params
-				'display_blog_title'   => false,
-				'open_children_levels' => 20,
-				'class_selected'       => '',
-				'item_before_opened'   => get_icon( 'collapse' ),
-				'item_before_closed'   => get_icon( 'expand' ),
-				'item_before_post'     => get_icon( 'file_message' ),
-			) );
-		// ---------------------------------- END OF CONTENT HIERARCHY ---------------------------------
-
-		echo '</div>';
-	}
-	// ----------------- END OF 404 CONTENT TEMPLATE INCLUDED HERE -------------------
+// -------------- MAIN CONTENT TEMPLATE INCLUDED HERE (Based on $disp) --------------
+skin_include( '$disp$' );
+// Note: you can customize any of the sub templates included here by
+// copying the matching php file into your skin directory.
+// ------------------------- END OF MAIN CONTENT TEMPLATE ---------------------------
 ?>
 
 
@@ -269,6 +209,10 @@ siteskin_include( '_site_body_header.inc.php' );
 
 			<div id="evo_container__sidebar">
 
+				<?php
+				if( $Skin->is_visible_container( 'sidebar' ) )
+				{ // Display 'Sidebar' widget container
+				?>
 				<div class="evo_container evo_container__sidebar">
 				<?php
 					// <div data-spy="affix" data-offset-top="165" class="affix_block">
@@ -305,7 +249,12 @@ siteskin_include( '_site_body_header.inc.php' );
 					// ----------------------------- END OF "Sidebar" CONTAINER -----------------------------
 				?>
 				</div>
+				<?php } ?>
 
+				<?php
+				if( $Skin->is_visible_container( 'sidebar2' ) )
+				{ // Display 'Sidebar 2' widget container
+				?>
 				<div class="evo_container evo_container__sidebar2">
 				<?php
 					// <div data-spy="affix" data-offset-top="165" class="affix_block">
@@ -342,6 +291,7 @@ siteskin_include( '_site_body_header.inc.php' );
 					// ----------------------------- END OF "Sidebar" CONTAINER -----------------------------
 				?>
 				</div>
+				<?php } ?>
 
 				<?php
 					// Please help us promote b2evolution and leave this logo on your blog:
@@ -368,6 +318,10 @@ siteskin_include( '_site_body_header.inc.php' );
 	<!-- =================================== START OF FOOTER =================================== -->
 	<div class="col-md-12 center">
 
+		<?php
+		if( $Skin->is_visible_container( 'footer' ) )
+		{ // Display 'Footer' widget container
+		?>
 		<div class="evo_container evo_container__footer">
 		<?php
 			// Display container and contents:
@@ -379,6 +333,7 @@ siteskin_include( '_site_body_header.inc.php' );
 			// Note: Double quotes have been used around "Footer" only for test purposes.
 		?>
 		</div>
+		<?php } ?>
 
 		<p>
 			<?php
@@ -432,9 +387,8 @@ siteskin_include( '_site_body_header.inc.php' );
 siteskin_include( '_site_body_footer.inc.php' );
 // ------------------------------- END OF SITE FOOTER --------------------------------
 
+
 // ------------------------- HTML FOOTER INCLUDED HERE --------------------------
 skin_include( '_html_footer.inc.php' );
-// Note: You can customize the default HTML footer by copying the
-// _html_footer.inc.php file into the current skin folder.
 // ------------------------------- END OF FOOTER --------------------------------
 ?>
